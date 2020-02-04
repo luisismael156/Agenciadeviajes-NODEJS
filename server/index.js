daf916a587e3772a432bc65c104c50aa7be4d2c7
ismael156 <luis.ifh12@gmail.com>
2020-02-03 23:37:49 -0500

    Proyecto Terminado

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-const configs = require('./config');
-const db = require('./config/database')
+const configs = require("./config");
+const db = require("./config/database");
+require("dotenv").config({ path: "variables.env" });
 
-db.authenticate().then(() => console.log("conectado jeje")).catch(error=> console.log(console.error()));
+db.authenticate()
+  .then(() => console.log("conectado jeje"))
+  .catch(error => console.log(console.error()));
 // confiurar expreress
 const app = express();
 
@@ -19,26 +22,31 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
 //cargar una carpeta estatica llamda public
-app.use(express.static("public"))
-
+app.use(express.static("public"));
 
 //valida si estamos en desarrollo  o production
 
-const config = configs[app.get('env')]
+const config = configs[app.get("env")];
 
-app.locals.titulo = config.nombresito
+app.locals.titulo = config.nombresito;
 
 //Muestra el año actual
 app.use((req, res, next) => {
-  const fecha = new Date()
-  res.locals.fechaActual = fecha.getFullYear()
+  const fecha = new Date();
+  res.locals.fechaActual = fecha.getFullYear();
   res.locals.ruta = req.path;
   return next();
-})
+});
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //cargar rutas
 app.use("/", routes());
 
-app.listen(3000);
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+//puerot y host para alppa
+app.listen(port, host, () => {
+  console.log("servidor esta funcionando");
+});
